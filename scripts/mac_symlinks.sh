005e4b9b1362553fcff903150b58c7bf8f7f869f
martin-fitger <57917288+martin-fitger@users.noreply.github.com>
2024-01-20 09:36:00 +0100

    Added symlink script.

---
+++ scripts/mac_symlinks.sh
@@ -0,0 +1,7 @@
+ln -s ../../../../res/logo/logo_16x16.png ../xcode/jass/Assets.xcassets/AppIcon.appiconset/logo_16x16.png
+ln -s ../../../../res/logo/logo_32x32.png ../xcode/jass/Assets.xcassets/AppIcon.appiconset/logo_32x32.png
+ln -s ../../../../res/logo/logo_64x64.png ../xcode/jass/Assets.xcassets/AppIcon.appiconset/logo_64x64.png
+ln -s ../../../../res/logo/logo_128x128.png ../xcode/jass/Assets.xcassets/AppIcon.appiconset/logo_128x128.png
+ln -s ../../../../res/logo/logo_256x256.png ../xcode/jass/Assets.xcassets/AppIcon.appiconset/logo_256x256.png
+ln -s ../../../../res/logo/logo_512x512.png ../xcode/jass/Assets.xcassets/AppIcon.appiconset/logo_512x512.png
+ln -s ../../../../res/logo/logo_1024x1024.png ../xcode/jass/Assets.xcassets/AppIcon.appiconset/logo_1024x1024.png
